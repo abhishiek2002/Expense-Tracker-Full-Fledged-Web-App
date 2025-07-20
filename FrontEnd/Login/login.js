c666ad594e5daad569a19abad8fc5323d727eafb
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-07-20 18:19:17 +0530

    Login Page Complete

---
+++ FrontEnd/Login/login.js
@@ -0,0 +1,23 @@
+import userClassInstance from "../Services/userServices.js";
+
+const loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const email = loginForm.email.value;
+  const password = loginForm.password.value;
+
+  try {
+    const res = await userClassInstance.login({ email, password });
+  } catch (error) {
+    let errorMessage;
+    if (error.response) {
+      errorMessage = error.response.data.error;
+    } else {
+      errorMessage = error.message;
+    }
+
+    const loginErrorField = document.getElementById("loginErrorField");
+    loginErrorField.textContent = errorMessage;
+  }
+});
