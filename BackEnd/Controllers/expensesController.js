9a0097cc2607f0bc91a29dfa8f157b0b788d1337
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-07-31 19:09:02 +0530

    add transaction functionality in all database queries

--- BackEnd/Controllers/expensesController.js
+++ BackEnd/Controllers/expensesController.js
@@ -16,13 +16,16 @@ async function addExpense(req, res) {
 
   try {
     // update the expense table with the new expense
-    const expense = await Expense.create({
-      title,
-      amount,
-      description,
-      category,
-      UserId: user.id,
-    }, { transaction });
+    const expense = await Expense.create(
+      {
+        title,
+        amount,
+        description,
+        category,
+        UserId: user.id,
+      },
+      { transaction }
+    );
 
     // update the user table with the new expense
     await User.increment(
@@ -32,7 +35,7 @@ async function addExpense(req, res) {
       },
       {
         where: { id: user.id },
-        transaction
+        transaction,
       }
     );
 
@@ -43,7 +46,6 @@ async function addExpense(req, res) {
       message: "Expense has been recorded",
       expense,
     });
-
   } catch (error) {
     transaction.rollback();
     console.error("Error while adding expense:", error);
@@ -77,15 +79,46 @@ async function deleteExpense(req, res) {
   const user = req.user;
   const id = req.params.id; // id of expense
 
+  const transaction = await sequelize.transaction();
+
   try {
     const expense = await Expense.destroy({
       where: { id: id, UserId: user.id },
+      transaction,
     });
+
+    // If the expense was not found or the user does not have permission to delete it
+    if (!expense) {
+      return res.status(404).json({
+        success: false,
+        message:
+          "Expense not found or you do not have permission to delete it.",
+      });
+    }
+
+    await User.decrement(
+      {
+        totalExpense: expense.amount, // Decrement the total expense by the amount of the deleted expense
+        expenseCount: 1, // Decrement monthly expense count
+      },
+      {
+        where: { id: user.id },
+        transaction,
+      }
+    );
+
+    // If the expense was successfully deleted
+    // Commit the transaction if using one, otherwise just send the response
+
+    transaction.commit();
+
     res.status(200).json({
       success: true,
       message: "Expense has been deleted successfully",
     });
   } catch (error) {
+    transaction.rollback();
+    console.error("Error while deleting expense:", error);
     res.status(502).json({
       error: error.message,
     });
